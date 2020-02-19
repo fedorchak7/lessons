e8d8d7f450a2f8d7f6208996bb35c088890445f6
My name <fedorchak01@gmail.ua>
2020-02-19 17:09:31 +0200

    Change menu

--- firstlesson.js
+++ firstlesson.js
@@ -1,24 +1,33 @@
-
-
-
-
-
 'use strict';
- // scroll top button
-// $(window).scroll(function(){
-// 	if ($(this).scrollTop() > 200){
-// 		$('#myBtn').fadeIn();
-// 	}else{
-//      $('#myBtn').fadeOut();
-// 	}
-// });
+// scroll top button
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 200) {
+        $('#myBtn').fadeIn();
+    } else {
+        $('#myBtn').fadeOut();
+    }
+});
 
-// $(document).on('click', $('#myBtn'), function(){
-// 	$('body, html').animate({
-// 		scrollTop: 0
-// 	}, 1500);
-// 	return false;
-// })
+$('#myBtn').click(function() {
+    $('body, html').animate({
+        scrollTop: 0
+    }, 1500);
+    return false;
+})
+
+
+$("#test").on('submit', function(event) {
+    alert("Handler for .submit() called.");
+    event.preventDefault();
+});
+
+
+// $(document).on('click', $('#myBtn'), function() {
+//     $('body, html').animate({
+//         scrollTop: 0
+//     }, 1500);
+//     return false;
+// });
 
 // $(document).on('click', $('.anchor'), function(e){
 // 	e.preventDefault();
@@ -29,10 +38,10 @@
 // 	}, 'slow');
 // });
 
-$(document).ready(function(){
-  $('.slider-wrapper').slick({
-  infinite: true,
-  slidesToShow: 3,
-  slidesToScroll: 3
-});
+$(document).ready(function() {
+    $('.slider-wrapper').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1
+    });
 });
