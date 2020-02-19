6bcd717f658e7cd39a5b41d745c57a6cb685fd78
My name <fedorchak01@gmail.ua>
2020-02-19 18:38:21 +0200

    Add validation

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 'use strict';
 // scroll top button
-$(window).scroll(function () {
+$(window).scroll(function() {
     if ($(this).scrollTop() > 200) {
         $('#myBtn').fadeIn();
     } else {
@@ -15,14 +15,51 @@ $(window).scroll(function () {
 //     return false;
 // })
 
-
-$("#test").on('submit', function (event) {
-    alert("Handler for .submit() called.");
+$("#test").on('submit', function(event) {
     event.preventDefault();
+    var error = false;
+
+    var $this = $(this);
+
+    var pattern = /^([a-z0-9_\.-])+@[a-z0-9-]+\.([a-z]{2,6}\.)?[a-z]{2,6}$/i;
+
+    var inputRequired = $this.find('.input-required'),
+        inputEmail = $this.find('.input-email'),
+        inputPhone = $this.find('.input-phone');
+
+    $('div.input-box').removeClass('error-box');
+
+    if (inputRequired.length > 0) {
+        inputRequired.each(function() {
+            if ($(this).find('input').val().trim() === '') {
+                $(this).closest('.input-box').addClass('error-box');
+                error = true;
+            }
+        })
+    }
+
+    if (inputPhone.length > 0) {
+        if (inputPhone.find('input').val().trim().length < 13) {
+            inputPhone.addClass('error-box');
+            error = true;
+        }
+    }
+
+    if (inputEmail.length > 0) {
+        if (!pattern.test(inputEmail.find('input').val())) {
+            inputEmail.addClass('error-box');
+            error = true;
+            console.log('ddd');
+        }
+    }
+
+    if (!error) {
+        console.log('submit done');
+    }
 });
 
 
-$(document).on('click', '#myBtn', function () {
+$(document).on('click', '#myBtn', function() {
     $('body, html').animate({
         scrollTop: 0
     }, 1500);
@@ -38,7 +75,7 @@ $(document).on('click', '#myBtn', function () {
 // 	}, 'slow');
 // });
 
-$(document).ready(function () {
+$(document).ready(function() {
     $('.slider-wrapper').slick({
         infinite: true,
         slidesToShow: 3,
